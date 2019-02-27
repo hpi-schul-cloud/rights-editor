0614b9d76aac5997a6cc1a2c3969eb85199ac8ba
Marco <mxrcx.mail@web.de>
2019-02-27 10:37:32 +0100

    added cc licence text

--- src/components/ChooserEditor.vue
+++ src/components/ChooserEditor.vue
@@ -6,32 +6,32 @@
         <p class="caption">Reproduktion</p>
         <p class="description">Das Werk darf reproduziert werden.</p>
         <div class="button-group">
-            <button class="positive" v-bind:class="{active: reproduction}" v-on:click="reproduction = true">Erlaubt</button>
-            <button class="negative" v-bind:class="{active: !reproduction}" v-on:click="reproduction = false;">Nicht erlaubt</button>
+            <button class="positive" v-bind:class="{active: reproduction}" v-on:click="reproduction = true; changeText()">Erlaubt</button>
+            <button class="negative" v-bind:class="{active: !reproduction}" v-on:click="reproduction = false; changeText();">Nicht erlaubt</button>
         </div>
     </div>
     <div class="rule">
         <p class="caption">Distribution</p>
         <p class="description">Das Werk (und, sofern authorisiert, Derivate) dürfen verteilt, öffentlich gezeigt und öffentlich aufgeführt werden.</p>
         <div class="button-group">
-            <button class="positive" v-bind:class="{active: distribution}" v-on:click="distribution = true">Erlaubt</button>
-            <button class="negative" v-bind:class="{active: !distribution}" v-on:click="distribution = false;">Nicht erlaubt</button>
+            <button class="positive" v-bind:class="{active: distribution}" v-on:click="distribution = true; changeText()">Erlaubt</button>
+            <button class="negative" v-bind:class="{active: !distribution}" v-on:click="distribution = false; changeText();">Nicht erlaubt</button>
         </div>
     </div>
     <div class="rule">
         <p class="caption">Derivat-Werke</p>
         <p class="description">Derivate des Werks dürfen erstellt und reproduziert werden.</p>
         <div class="button-group">
-            <button class="positive" v-bind:class="{active: derivateWorks}" v-on:click="derivateWorks = true">Erlaubt</button>
-            <button class="negative" v-bind:class="{active: !derivateWorks}" v-on:click="derivateWorks = false;">Nicht erlaubt</button>
+            <button class="positive" v-bind:class="{active: derivateWorks}" v-on:click="derivateWorks = true; changeText()">Erlaubt</button>
+            <button class="negative" v-bind:class="{active: !derivateWorks}" v-on:click="derivateWorks = false; changeText();">Nicht erlaubt</button>
         </div>
     </div>
     <div class="rule">
         <p class="caption">Teilen</p>
         <p class="description">Nichtkommerzielle Reproduktion und Distribution (wie z.B. File-Sharing) des gesamten Werks ist erlaubt.</p>
         <div class="button-group">
-            <button class="positive" v-bind:class="{active: sharing}" v-on:click="sharing = true">Erlaubt</button>
-            <button class="negative" v-bind:class="{active: !sharing}" v-on:click="sharing = false;">Nicht erlaubt</button>
+            <button class="positive" v-bind:class="{active: sharing}" v-on:click="sharing = true; changeText()">Erlaubt</button>
+            <button class="negative" v-bind:class="{active: !sharing}" v-on:click="sharing = false; changeText();">Nicht erlaubt</button>
         </div>
     </div>
     <h2>Einschränkungen</h2>
@@ -39,8 +39,8 @@
         <p class="caption">Nichtkommerziele Nutzung</p>
         <p class="description">Die eingräumten Berechtigungen dürfen nicht für kommerzielle Zwecke genutzt werden.</p>
         <div class="button-group">
-            <button class="positive" v-bind:class="{active: nonCommercialUse}" v-on:click="nonCommercialUse = true">Eingeschränkt</button>
-            <button class="negative" v-bind:class="{active: !nonCommercialUse}" v-on:click="nonCommercialUse = false;">Nicht Eingeschränkt</button>
+            <button class="positive" v-bind:class="{active: nonCommercialUse}" v-on:click="nonCommercialUse = true; changeText()">Eingeschränkt</button>
+            <button class="negative" v-bind:class="{active: !nonCommercialUse}" v-on:click="nonCommercialUse = false; changeText();">Nicht Eingeschränkt</button>
         </div>
     </div>
     <h2>Anforderung</h2>
@@ -48,27 +48,27 @@
         <p class="caption">Lizenzangabe</p>
         <p class="description">Copyright- und Lizenzangaben müssen mit dem Werk zusammen bleiben.</p>
         <div class="button-group">
-            <button class="positive" v-bind:class="{active: notice}" v-on:click="notice = true">Verlangt</button>
-            <button class="negative" v-bind:class="{active: !notice}" v-on:click="notice = false;">Nicht verlangt</button>
+            <button class="positive" v-bind:class="{active: notice}" v-on:click="notice = true; changeText()">Verlangt</button>
+            <button class="negative" v-bind:class="{active: !notice}" v-on:click="notice = false; changeText();">Nicht verlangt</button>
         </div>
     </div>
     <div class="rule">
         <p class="caption">Lizenzbeibehaltung</p>
         <p class="description">Derivate müssen mit gleichen Bedingungen lizenziert werden wie das originale Werk.</p>
         <div class="button-group">
-            <button class="positive" v-bind:class="{active: shareAlike}" v-on:click="shareAlike = true">Verlangt</button>
-            <button class="negative" v-bind:class="{active: !shareAlike}" v-on:click="shareAlike = false;">Nicht verlangt</button>
+            <button class="positive" v-bind:class="{active: shareAlike}" v-on:click="shareAlike = true; changeText()">Verlangt</button>
+            <button class="negative" v-bind:class="{active: !shareAlike}" v-on:click="shareAlike = false; changeText();">Nicht verlangt</button>
         </div>
     </div>
     <div v-if="notice" class="rule">
         <p class="caption">CC-Lizenz</p>
-        <p class="description">Die erstellte CC-Lizenz:</p>
-        <img v-if="notice && !nonCommercialUse && !shareAlike && derivateWorks" src="../img/cc/by.png">
-        <img v-if="notice && !nonCommercialUse && shareAlike && derivateWorks" src="../img/cc/by-sa.png">
-        <img v-if="notice && !nonCommercialUse && !shareAlike && !derivateWorks" src="../img/cc/by-nd.png">
-        <img v-if="notice && nonCommercialUse && !shareAlike && derivateWorks" src="../img/cc/by-nc.eu.png">
-        <img v-if="notice && nonCommercialUse && shareAlike && derivateWorks" src="../img/cc/by-nc-sa.eu.png">
-        <img v-if="notice && nonCommercialUse && !shareAlike && !derivateWorks" src="../img/cc/by-nc-nd.eu.png">
+        <p id="imageText" class="description">Die erstellte CC-Lizenz: {{ imageText }}</p>
+        <img id="img1" v-if="notice && !nonCommercialUse && !shareAlike && derivateWorks" src="../img/cc/by.png">
+        <img id="img2" v-if="notice && !nonCommercialUse && shareAlike && derivateWorks" src="../img/cc/by-sa.png">
+        <img id="img3" v-if="notice && !nonCommercialUse && !shareAlike && !derivateWorks" src="../img/cc/by-nd.png">
+        <img id="img4" v-if="notice && nonCommercialUse && !shareAlike && derivateWorks" src="../img/cc/by-nc.eu.png">
+        <img id="img5" v-if="notice && nonCommercialUse && shareAlike && derivateWorks" src="../img/cc/by-nc-sa.eu.png">
+        <img id="img6" v-if="notice && nonCommercialUse && !shareAlike && !derivateWorks" src="../img/cc/by-nc-nd.eu.png">
     </div>
 </div>   
 </template>
@@ -85,8 +85,31 @@ export default {
             nonCommercialUse: false,
             notice: true,
             shareAlike: false,
+            imageText: "Namensnennung 4.0 International"
         };
     },
+    methods: {
+        changeText() {
+            if(document.getElementById("img1")) {
+                this.imageText = "Namensnennung 4.0 International";
+            }
+            else if(document.getElementById("img2")) {
+                this.imageText = "Namensnennung-Share Alike 4.0 International";
+            }
+            else if(document.getElementById("img3")) {
+                this.imageText = "Namensnennung-Keine Bearbeitungen 4.0 International";
+            }
+            else if(document.getElementById("img4")) {
+                this.imageText = "Namensnennung-Nicht kommerziell 4.0 International";
+            }
+            else if(document.getElementById("img5")) {
+                this.imageText = "Namensnennung-Nicht kommerziell-Share Alike 4.0 International";
+            }
+            else if(document.getElementById("img6")) {
+                this.imageText = "Namensnennung-Nicht kommerziell-Keine Bearbeitungen 4.0 International";
+            }          
+        }
+    }
 };
 </script>
 
