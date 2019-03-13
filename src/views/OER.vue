ec7476c78d38e703b34cd5a28fced84e4e8af32b
Hannes Kohlsaat <hkohlsaat@gmail.com>
2019-03-13 15:00:55 +0100

    multiple pages with vue-router

---
+++ src/views/OER.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="oer-page">
+  <ChooserEditor></ChooserEditor>
+</div>
+</template>
+
+<script>
+import ChooserEditor from '../components/ChooserEditor.vue';
+
+export default {
+  name: 'oer',
+  components: {
+    ChooserEditor
+  },
+  data: function() {
+    return {
+    };
+  },
+}
+</script>
+
+
