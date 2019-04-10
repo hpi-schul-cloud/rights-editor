19458de34e4f9b1e558411830f8ac746d5e3c3ad
Hannes Kohlsaat <hkohlsaat@gmail.com>
2019-04-10 13:20:12 +0200

    Policy tree (#56)
    * adding PolicyTree and PolicyTreeRuleItem
    
    * connecting the tree and the rule items

--- src/components/PolicyTree/PolicyTree.vue
+++ src/components/PolicyTree/PolicyTree.vue
@@ -1,51 +1,63 @@
 <template>
-    <div>
-        <p class="label">Policy</p>
+  <div>
+    <p class="label">
+      Policy
+    </p>
 
-        <div class="rules" v-if="policy['permission']">
-            <PolicyTreeRuleItem
-                v-for="(permission, index) in policy['permission']"
-                :key="index"
-                :policy="policy"
-                :path="['permission', index]"
-            /> 
-        </div>
-
-        <div v-if="policy['obligation']" class="rules" >
-            <PolicyTreeRuleItem
-                v-for="(obligation, index) in policy['obligation']"
-                :key="index"
-                :policy="policy"
-                :path="['obligation', index]"
-            /> 
-        </div>
-
-        <div v-if="policy['prohibition']" class="rules" >
-            <PolicyTreeRuleItem
-                v-for="(prohibition, index) in policy['prohibition']"
-                :key="index"
-                :policy="policy"
-                :path="['prohibition', index]"
-            /> 
-        </div>
+    <div v-if="policy['permission']" class="rules">
+      <PolicyTreeRuleItem
+        v-for="(permission, index) in policy['permission']"
+        :key="index"
+        :policy="policy"
+        :path="['permission', index]"
+        :selectedPath="selectedPath"
+        @update:selectedPath="$emit('update:selectedPath', $event)"
+      />
     </div>
+
+    <div v-if="policy['obligation']" class="rules">
+      <PolicyTreeRuleItem
+        v-for="(obligation, index) in policy['obligation']"
+        :key="index"
+        :policy="policy"
+        :path="['obligation', index]"
+        :selectedPath="selectedPath"
+        @update:selectedPath="$emit('update:selectedPath', $event)"
+      />
+    </div>
+
+    <div v-if="policy['prohibition']" class="rules">
+      <PolicyTreeRuleItem
+        v-for="(prohibition, index) in policy['prohibition']"
+        :key="index"
+        :policy="policy"
+        :path="['prohibition', index]"
+        :selectedPath="selectedPath"
+        @update:selectedPath="$emit('update:selectedPath', $event)"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
 import PolicyTreeRuleItem from './PolicyTreeRuleItem';
 
 export default {
-    name: 'PolicyTree',
-    components: {
-        PolicyTreeRuleItem,
+  name: 'PolicyTree',
+  components: {
+    PolicyTreeRuleItem,
+  },
+  props: {
+    policy: {
+      type: Object,
+      required: true,
     },
-    props: {
-        policy: {
-            type: Object,
-            required: true,
-        },
+    selectedPath: {
+      type: Array,
+      required: true,
     },
-}
+  },
+};
 </script>
 
 <style scoped>
--- src/components/PolicyTree/PolicyTreeRuleItem.vue
+++ src/components/PolicyTree/PolicyTreeRuleItem.vue
@@ -1,53 +1,63 @@
 <template>
-    <div>
-        <p class="label">{{ name }}</p>
-        
-        <div class="subrules" v-if="subrules">
-            <PolicyTreeRuleItem
-                v-for="(subrule, index) in subrules"
-                :key="index"
-                :policy="policy"
-                :path="[...path, subruleType, index]"
-            />
-        </div>
+  <div>
+    <p class="label" :class="{ selectedRule: isSelected }" @click="$emit('update:selectedPath', path)">
+      {{ name }}
+    </p>
 
+    <div v-if="subrules" class="subrules">
+      <PolicyTreeRuleItem
+        v-for="(subrule, index) in subrules"
+        :key="index"
+        :policy="policy"
+        :path="[...path, subruleType, index]"
+        :selectedPath="selectedPath"
+        @update:selectedPath="$emit('update:selectedPath', $event)"
+      />
     </div>
+  </div>
 </template>
 
 <script>
 import { RuleTypes } from '../../libs/rules/rules';
 
 export default {
-    name: 'PolicyTreeRuleItem',
-    props: {
-        policy: {
-            type: Object,
-            required: true,
-        },
-        path: {
-            type: Array,
-            required: true,
-        }
+  name: 'PolicyTreeRuleItem',
+  props: {
+    policy: {
+      type: Object,
+      required: true,
     },
-    computed: {
-        rule() {
-            return this.path.reduce((result, segment) => result[segment], this.policy);
-        },
-        name() {
-            let pathLen = this.path.length;
-            let rType = RuleTypes[this.path[pathLen - 2]];
-            return rType.name;
-        },
-        subruleType() {
-            let pathLen = this.path.length;
-            let rType = RuleTypes[this.path[pathLen - 2]];
-            return rType.subrule;
-        },
-        subrules() {
-            return this.rule[this.subruleType];
-        }
+    path: {
+      type: Array,
+      required: true,
     },
-}
+    selectedPath: {
+      type: Array,
+      required: true,
+    },
+  },
+  computed: {
+    rule() {
+      return this.path.reduce((result, segment) => result[segment], this.policy);
+    },
+    name() {
+      const pathLen = this.path.length;
+      const rType = RuleTypes[this.path[pathLen - 2]];
+      return rType.name;
+    },
+    subruleType() {
+      const pathLen = this.path.length;
+      const rType = RuleTypes[this.path[pathLen - 2]];
+      return rType.subrule;
+    },
+    subrules() {
+      return this.rule[this.subruleType];
+    },
+    isSelected() {
+      return this.path.join() === this.selectedPath.join();
+    }
+  },
+};
 </script>
 
 <style scoped>
@@ -56,5 +66,9 @@ export default {
 }
 .subrules {
     padding-left: 20px;
+}
+.selectedRule {
+  color: #1f3b70;
+  font-weight: bold;
 }
 </style>
