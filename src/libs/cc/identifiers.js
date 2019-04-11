a8ed5880fdd9ab7c10a8cb62b20e2542fbbb396b
CarolinGoerke <carolingoerke@gmx.de>
2019-04-11 13:02:10 +0200

    rest of the identifiers

--- src/libs/cc/identifiers.js
+++ src/libs/cc/identifiers.js
@@ -7,10 +7,10 @@ export const ID_BY_NC_SA = 'http://creativecommons.org/licenses/by-nc-sa/4.0/';
 
 let nameMapping = {};
 nameMapping[ID_BY] = 'Namensnennung';
-nameMapping[ID_BY_ND] = 'Namensnennung - Weitergabe unter gleichen Bedingungen';
+nameMapping[ID_BY_ND] = 'Namensnennung-Keine Bearbeitung';
 nameMapping[ID_BY_SA] = 'Namensnennung - Weitergabe unter gleichen Bedingungen';
-nameMapping[ID_BY_NC] = 'Namensnennung - Weitergabe unter gleichen Bedingungen';
-nameMapping[ID_BY_NC_ND] = 'Namensnennung - Weitergabe unter gleichen Bedingungen';
-nameMapping[ID_BY_NC_SA] = 'Namensnennung - Weitergabe unter gleichen Bedingungen';
+nameMapping[ID_BY_NC] = 'Namensnennung-Nicht kommerziell';
+nameMapping[ID_BY_NC_ND] = 'Namensnennung - Nicht-kommerziell - Keine Bearbeitung';
+nameMapping[ID_BY_NC_SA] = 'Namensnennung - Nicht-kommerziell - Weitergabe unter gleichen Bedingungen';
 
 export const ccNames = nameMapping;
