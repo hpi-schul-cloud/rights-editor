0ecdfbe8afa73ca3682697dde2b93d732f7a6725
Hannes Kohlsaat <hkohlsaat@gmail.com>
2019-04-11 11:28:17 +0200

    Rule item refactoring (#62)
    * policy routing
    
    * refactoring RuleItem
    
    * fixing bugs
    
    * forgot to save before merge 🤫

---
+++ src/components/EmbedInText.vue
@@ -0,0 +1,23 @@
+<template>
+    <span>
+        {{ textBefore }}
+        <slot />
+        {{ textAfter }}
+    </span>
+</template>
+
+<script>
+export default {
+    name: 'EmbedInText',
+    props: {
+        textBefore: {
+            type: String,
+            default: '',
+        },
+        textAfter: {
+            type: String,
+            default: '',
+        },
+    },
+}
+</script>
