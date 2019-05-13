e0154c6decf9b19f6c41c1fd99069f1aa897968e
Hannes Kohlsaat <hkohlsaat@gmail.com>
2019-05-13 16:11:00 +0200

    ideas for implementation of basic functionality

--- src/components/PresetEditor.vue
+++ src/components/PresetEditor.vue
@@ -9,25 +9,61 @@
     </EditorNavBar>
     <h1>Regel-Editor</h1>
     <ul class="rule-list">
-      <li>Drucken</li>
-      <li class="selected">Kopieren</li>
-      <li>Drucken</li>
-      <li>Drucken</li>
-      <li class="active">Kopieren</li>
-      <li>Drucken</li>
+      <li v-for="(rule, index) in rules" :key="index" @click="activeRuleIndex = index" :class="{ selected: selectedRules.indexOf(index) >= 0 ,active: activeRuleIndex === index }">{{ rule.name }}</li>
     </ul>
+    <div class="details" v-if="activeRule">
+      <h2>{{ activeRule.name }}</h2>
+      <p>{{ activeRule.description }}{{ activeRule.name }}.</p>
+      <BaseButton v-if="selectedRules.indexOf(activeRuleIndex) >= 0" @click="removeRule(activeRuleIndex)">Regel entfernen</BaseButton>
+      <BaseButton v-else @click="addRule(activeRuleIndex)">Regel hinzufügen</BaseButton>
+    </div>
   </div>
 </template>
 
 
 <script>
 import EditorNavBar from './EditorNavBar.vue';
+import BaseButton from './BaseButton.vue';
+import Vue from 'vue';
+
+
+const rules = [
+  { name: 'Nichtkommerzielle, öffentliche Vorführung', description: 'TODO: Dies ist die Beschreibung von '},
+  { name: 'Download', description: 'TODO: Dies ist die Beschreibung von '},
+  { name: 'Streaming', description: 'TODO: Dies ist die Beschreibung von '}, 
+  { name: 'Speichern', description: 'TODO: Dies ist die Beschreibung von '},
+  { name: 'Remix', description: 'TODO: Dies ist die Beschreibung von '},
+  { name: 'Thumbnailerzeugung', description: 'TODO: Dies ist die Beschreibung von '},
+];
 
 export default {
   name: 'PresetEditor',
   components: {
     EditorNavBar,
+    BaseButton,
   },
+  data() {
+    return {
+      activeRuleIndex: -1,
+      selectedRules: [],
+    };
+  },
+  computed: {
+    rules() {
+      return rules;
+    },
+    activeRule() {
+      return rules[this.activeRuleIndex];
+    }
+  },
+  methods: {
+    addRule(index) {
+      this.selectedRules.push(index);
+    },
+    removeRule(index) {
+      Vue.delete(this.selectedRules, this.selectedRules.indexOf(index));
+    },
+  }
 };
 </script>
 
@@ -38,24 +74,41 @@ export default {
   box-sizing: border-box;
   box-shadow: 3px 0px 2px -3px gray;
   padding-right: 8px;
+  margin: 0px;
   /* padding-bottom: 12px; */
 }
 
-.rule-list li {
+li {
     padding: 12px 4px;
+    /* margin: 8px 0px; */
+    border-top: 1px solid gray;
     border-bottom: 1px solid gray;
+    margin-bottom: 8px;
+    cursor: pointer;
     /* background-color: #eee; */
 }
 
-li.active {
-    color: #1f3b70;
-    font-weight: bold;
-    /* font-style: italic; */
-    border-bottom: 1px solid #1f3b70;
+li:last-child {
+  margin: 0px;
+}
 
+.active {
+    color: #1f3b70;
+    font-style: italic;
+    border-top: 1px solid #1f3b70;
+    border-bottom: 1px solid #1f3b70;
 }
 
 .selected {
-
+  color: #1f3b70;
+    font-weight: bold;
+    border-top: 1px solid #1f3b70;
+    border-bottom: 1px solid #1f3b70;
+    padding-left: 12px;
 }
+
+.details {
+  margin-left: 232px;
+}
+
 </style>
