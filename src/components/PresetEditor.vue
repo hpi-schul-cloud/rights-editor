f3b7cb12e327381aa0bf98975250c45605f65ae3
Hannes Kohlsaat <hkohlsaat@gmail.com>
2019-05-14 16:09:26 +0200

    design changes

--- src/components/PresetEditor.vue
+++ src/components/PresetEditor.vue
@@ -8,32 +8,47 @@
       </template>
     </EditorNavBar>
     <h1>Regel-Editor</h1>
-    <ul class="rule-list">
-      <li v-for="(rule, index) in rules" :key="index" @click="activeRuleIndex = index" :class="{ selected: selectedRules.indexOf(index) >= 0 ,active: activeRuleIndex === index }">{{ rule.name }}</li>
-    </ul>
-    <div class="details" v-if="activeRule">
-      <h2>{{ activeRule.name }}</h2>
-      <p>{{ activeRule.description }}{{ activeRule.name }}.</p>
-      <BaseButton v-if="selectedRules.indexOf(activeRuleIndex) >= 0" @click="removeRule(activeRuleIndex)">Regel entfernen</BaseButton>
-      <BaseButton v-else @click="addRule(activeRuleIndex)">Regel hinzufügen</BaseButton>
+    <div class="info">
+      <div style="height: 30px; width: 20px; float: left"><i class="fas fa-info" /></div>
+      <p>Hier geht es darum, welche Aktionen später mit den Medien erlaubt sein sollen.
+        Suchen Sie aus der Liste links die Regeln, die Sie erlauben. Klicken Sie im Fenster
+        der jeweiligen Regel auf "Regel hinzufügen" um sie Ihrer Auswahl hinzuzufügen.</p>
     </div>
+    <table>
+      <tr v-for="(rule, index) in rules" :key="index" @click="activeRuleIndex = index">
+        <td><i :class="{ 'transparent-check': !rule.selected }" class="fas fa-check" /></td>
+        <td :class="{ selected: rule.selected,active: activeRuleIndex === index }">{{ rule.name }}</td>
+      </tr>
+    </table>
+    <div class="details">
+      <div v-if="activeRule">
+        <h2>{{ activeRule.name }}</h2>
+        <p>{{ activeRule.description }}{{ activeRule.name }}.</p>
+        <BaseButton v-if="selectedRules.indexOf(activeRuleIndex) >= 0" @click="removeRule(activeRuleIndex)">Regel entfernen</BaseButton>
+        <BaseButton v-else @click="addRule(activeRuleIndex)">Regel hinzufügen</BaseButton>
+      </div>
+      <p v-else class="placeholder">Es sind noch keine Regeln ausgewählt.</p>
+    </div>
+  </div>
   </div>
 </template>
 
 
 <script>
+import Vue from 'vue';
 import EditorNavBar from './EditorNavBar.vue';
 import BaseButton from './BaseButton.vue';
-import Vue from 'vue';
 
 
 const rules = [
-  { name: 'Nichtkommerzielle, öffentliche Vorführung', description: 'TODO: Dies ist die Beschreibung von '},
-  { name: 'Download', description: 'TODO: Dies ist die Beschreibung von '},
-  { name: 'Streaming', description: 'TODO: Dies ist die Beschreibung von '}, 
-  { name: 'Speichern', description: 'TODO: Dies ist die Beschreibung von '},
-  { name: 'Remix', description: 'TODO: Dies ist die Beschreibung von '},
-  { name: 'Thumbnailerzeugung', description: 'TODO: Dies ist die Beschreibung von '},
+  { name: 'Nicht-gewerbliche, öffentliche Vorführung', description: 'TODO: Dies ist die Beschreibung von ' },
+  { name: 'Download', description: 'TODO: Dies ist die Beschreibung von ' },
+  { name: 'Streaming', description: 'TODO: Dies ist die Beschreibung von ' },
+  { name: 'Speichern', description: 'TODO: Dies ist die Beschreibung von ' },
+  { name: 'Bearbeitung', description: 'TODO: Dies ist die Beschreibung von ' },
+  { name: 'Remix', description: 'TODO: Dies ist die Beschreibung von ' },
+  { name: 'Öffentliche Zugänglichmachung bearbeiteter Materialien', description: 'TODO: Dies ist die Beschreibung von ' },
+  { name: 'Thumbnailerzeugung', description: 'TODO: Dies ist die Beschreibung von ' },
 ];
 
 export default {
@@ -50,11 +65,14 @@ export default {
   },
   computed: {
     rules() {
-      return rules;
+      return rules.map((rule, index) => ({
+        name: rule.name,
+        selected: this.selectedRules.indexOf(index) >= 0,
+      }));
     },
     activeRule() {
       return rules[this.activeRuleIndex];
-    }
+    },
   },
   methods: {
     addRule(index) {
@@ -63,52 +81,69 @@ export default {
     removeRule(index) {
       Vue.delete(this.selectedRules, this.selectedRules.indexOf(index));
     },
-  }
+  },
 };
 </script>
 
 <style scoped>
-.rule-list {
+table {
+  border-collapse: collapse;
   float: left;
   width: 200px;
-  box-sizing: border-box;
   box-shadow: 3px 0px 2px -3px gray;
-  padding-right: 8px;
+}
+
+td {
   margin: 0px;
-  /* padding-bottom: 12px; */
-}
-
-li {
-    padding: 12px 4px;
-    /* margin: 8px 0px; */
-    border-top: 1px solid gray;
-    border-bottom: 1px solid gray;
-    margin-bottom: 8px;
-    cursor: pointer;
-    /* background-color: #eee; */
-}
-
-li:last-child {
-  margin: 0px;
-}
-
-.active {
-    color: #1f3b70;
-    font-style: italic;
-    border-top: 1px solid #1f3b70;
-    border-bottom: 1px solid #1f3b70;
+  padding: 8px 4px;
+  border-top: 1px solid transparent;
+  border-bottom: 1px solid transparent;
+  cursor: pointer;
 }
 
 .selected {
   color: #1f3b70;
+}
+
+.active {
+    color: #1f3b70;
     font-weight: bold;
-    border-top: 1px solid #1f3b70;
-    border-bottom: 1px solid #1f3b70;
-    padding-left: 12px;
+}
+
+p {
+  margin: 0px;
+}
+
+.fas {
+  color: #1f3b70;
+}
+.fas.transparent-check {
+  color: transparent;
+}
+
+li:hover .border {
+  border-bottom-color: transparent;
+}
+
+.info {
+  margin: 16px 0px;
+  padding: 16px;
+  border-radius: 16px;
+  border: 2px solid #1f3b70;
+  background-color: #f1f6fb;
 }
 
 .details {
   margin-left: 232px;
+  height: 360px;
+}
+
+.placeholder {
+  font-size: 30px;
+  font-style: italic;
+  color: gray;
+  text-align: center;
+  padding-top: 150px;
 }
 
 </style>
