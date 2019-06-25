5fb36d226a41e9f55e40f95369fcb8cd47f48f87
Hannes Kohlsaat <hkohlsaat@gmail.com>
2019-06-25 10:31:55 +0200

    splitting DimensionsEditor contents into two

--- src/router.js
+++ src/router.js
@@ -58,6 +58,7 @@ export default new Router({
       path: '/schul-cloud-save',
       name: 'sc-save',
       component: SCSaveScreen,
+      props: true,
     },    
     {
       path: '/filmsortiment-case-study',
